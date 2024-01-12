28738e434ae15fc3a9d87357c4c60ed564b41739
Tomasz Bielecki <szkoleniegoit@gmail.com>
2024-01-12 14:39:32 +0100

    completed work

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Profile from "./components/Profile/Profile.jsx";
+import user from "./data/user.json";
+import Statistics from "./components/Statistics/Statistics.jsx";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
+import FriendList from "./components/FriendsList/FriendList";
+import FriendListItem from "./components/FriendListItem/FriendListItem";
+import transactions from "./data/transactions.json";
+import TransactionHistory from "./components/Transactions/TransactionHistory";
 
-function App() {
-  const [count, setCount] = useState(0)
-  
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      statsFollower={user.stats.followers}
+      statsViews={user.stats.views}
+      statsLikes={user.stats.likes}
+    ></Profile>
 
-export default App
+    <Statistics stats={data} title={"Upload stats"} />
+
+    <FriendList>
+      <FriendListItem friends={friends} />
+    </FriendList>
+
+    <TransactionHistory items={transactions} />
+  </>
+);
+
+export default App;
